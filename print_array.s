aacf058e7823ed84e981e3e5606aa7d372f5f8fa
suparak <suparak.may@gmail.com>
2024-03-04 15:23:00 +0700

    print_array updated

--- print_array.s
+++ print_array.s
@@ -1,56 +1,60 @@
 .data
 A: .word 11, 22, 33, 44, 55
-B: .word 11, 22, 33, 44, 55, 66, 77, 88, 99, 1234
-newline: .string "\n"
-space: .string " "
+space: .asciiz " "
+newline: .asciiz "\n"
 
 .text
+.globl main
 main:
-    la a0, B # loading the starting address of array A to a0
-    addi a1, x0, 10 # passing the array size value to a1
+
+    # Passing the two arguments to a0 and a1
+    la a0, A
+    addi a1, x0, 5
+
+    # Jump to the print_array procedure
     jal print_array
-    
-    # exit cleanly
+
+    # Exit gracefully
     addi a0, x0, 10
-    ecall
+    addi a1, x0, 0
+    ecall  # Terminate ecall
 
 print_array:
-    addi t0, x0, 0 # let the i value be in t0
-loop1:
-    bge t0, a1, exit1
-    slli t1, t0, 2 # t1 has the i*4 value
-    add t2, t1, a0
-    lw t3, 0(t2) # t3 has the value of A[i]
+    addi sp, sp, -12
+    sw ra, 8(sp)        # Save ra register onto the stack
+    sw s1, 4(sp)
+    sw s0, 0(sp)
+    addi t0, x0, 0      # i = 0
     
-    # print A[i]
+    # Move parameters in a0 and a1 to callee-saved registers
+    mv s0, a0
+    mv s1, a1
     
-    # save a0 and a1 on to the stack; caller save as ecall realizes that a0 and a1 needed after the call
-    addi sp, sp, -8
-    sw a0, 0(sp)
-    sw a1, 4(sp)
-    
-    addi a0, x0, 1
-    mv a1, t3
-    ecall
+loop:
+    bge t0, s1, loop_exit  # Exit loop if i >= size
+    slli t1, t0, 2         # i = i * 4
+    add t1, s0, t1         # t1 = &A[0] + i*4
+    lw t1, 0(t1)           # t1 = A[i]
 
-    # print space
+    # printf("%d ", A[i])
+    addi a0, x0, 1
+    mv a1, t1
+    ecall
     addi a0, x0, 4
     la a1, space
     ecall
-    
-    # restoring a0 and a1 to their original value
-    lw a0, 0(sp)
-    lw a1, 4(sp)
-    addi sp, sp, 8
-    
-    addi t0, t0, 1 # i++
-    j loop1
-    
-exit1:
-    # print newline
+
+    addi t0, t0, 1         # i++
+    j loop                 # Goto loop
+
+loop_exit:
     addi a0, x0, 4
     la a1, newline
     ecall
-    
+
+    # Restore ra and return
+    lw ra, 8(sp)
+    lw s1, 4(sp)
+    lw s0, 0(sp)
+    addi sp, sp, 12
     jr ra
-    
